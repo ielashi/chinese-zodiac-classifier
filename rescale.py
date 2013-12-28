177101446b6dd971f0caa52ab1fdb0f1c2ce4a0b
Islam El-Ashi <islam@elashi.me>
2013-12-28 15:14:27 +0800

    Binarify dataset after resizing (not before).

--- rescale.py
+++ rescale.py
@@ -3,13 +3,13 @@ from visual import *
 
 
 dataset = load_dataset()
-dataset = make_binary(dataset)
 dataset = crop_bounding_box(dataset)
 dataset = resize_images(dataset, 20, 20)
+dataset = make_binary(dataset)
 
-draw_character(dataset[0][0])
-draw_character(dataset[1][0])
-draw_character(dataset[2][0])
-draw_character(dataset[3][0])
-draw_character(dataset[4][0])
+#draw_character(dataset[0][0])
+#draw_character(dataset[1][0])
+#draw_character(dataset[2][0])
+#draw_character(dataset[3][0])
+#draw_character(dataset[4][0])
 output_dataset(dataset)
